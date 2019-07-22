561c22b77a9e2aa5e3730e0b94996b2db4b02f7c
yangchenan <czx830109@gmail.com>
2019-07-22 22:01:04 +0800

    second commit

--- rand1.py
+++ rand1.py
@@ -1,12 +1,18 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count+1
 	guess = input('請猜數字:')
 	guess = int(guess)
 	if guess == r:
 		print('答對了')
+		print('你玩了', count, '次')	
 		break
 	elif guess > r:
 		print('太大')
 	elif guess < r:
 		print('太小')
+	print('你玩了', count, '次')	
+
+
